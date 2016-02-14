c54c6eeb0474a4a699a60582c7b79288d48c1be1
ccrock <cam.crockett.12@gmail.com>
2016-02-14 02:29:20 -0700

    Updating POM, adding DB connection, creating initial setup scripts

--- CloudSpeedTest/src/main/java/com/oracle/cloud/speedtest/dao/SpeedTestDao.java
+++ CloudSpeedTest/src/main/java/com/oracle/cloud/speedtest/dao/SpeedTestDao.java
@@ -1,5 +1,20 @@
 package com.oracle.cloud.speedtest.dao;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class SpeedTestDao {
 
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	@Qualifier(value = "dataSource")
+	public void setDataSource(DataSource dataSource) {
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
 }
