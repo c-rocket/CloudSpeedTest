9ce7ad515ceb4e695d5cad772222cfe86e059552
ccrock <cam.crockett.12@gmail.com>
2016-02-21 14:14:15 -0700

    Ignoring integration tests for now

--- CloudSpeedTest/src/test/java/com/oracle/cloud/speedtest/dao/SpeedTestDaoTest.java
+++ CloudSpeedTest/src/test/java/com/oracle/cloud/speedtest/dao/SpeedTestDaoTest.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -16,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml" })
 @Transactional
+@Ignore
 public class SpeedTestDaoTest {
 
 	@Resource
